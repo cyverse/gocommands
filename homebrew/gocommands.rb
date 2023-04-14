361836dfd998c26904af5d03e1fa5013d5283259
Illyoung Choi <iychoi@email.arizona.edu>
2023-04-14 11:01:08 -0700

    Fix audit problems in brew formula

--- homebrew/gocommands.rb
+++ homebrew/gocommands.rb
@@ -1,5 +1,5 @@
 class Gocommands < Formula
-  desc "Gocommands is a portable command-line toolkit for iRODS data management service"
+  desc "Portable command-line toolkit for iRODS data management service"
   homepage "https://github.com/cyverse/gocommands"
   url "https://github.com/cyverse/gocommands/archive/refs/tags/v0.6.4.tar.gz"
   sha256 "abd53221598df847a31904c86783aad36d160fbc97fc2da612e2f47919abd179"
@@ -11,16 +11,15 @@ class Gocommands < Formula
   end
 
   depends_on "go" => :build
-  
+
   def install
     gocmd_pkg = "github.com/cyverse/gocommands"
     gocmd_version = version.to_s
-    ldflags = "-X #{gocmd_pkg}/commons.clientVersion=v#{gocmd_version} -X #{gocmd_pkg}/commons.gitCommit=<HOMEBREW_RELEASE>"
+    ldflags = "-X #{gocmd_pkg}/commons.clientVersion=v#{gocmd_version}"
 
-    system "mkdir", "-p", "bin"
-    system "go", "build", "-ldflags", ldflags, "-o", "bin/gocmd", "cmd/gocmd.go"
+    system "go", "build", "-ldflags", ldflags, "-o", "gocmd", "cmd/gocmd.go"
 
-    bin.install "bin/gocmd"
+    bin.install "gocmd"
   end
 
   test do
