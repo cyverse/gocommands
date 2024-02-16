1e216f1465ab01ac162949c01491218f853da4c1
Illyoung Choi <iychoi@email.arizona.edu>
2024-02-15 17:05:09 -0700

    bump up version for homebrew

--- homebrew/gocommands.rb
+++ homebrew/gocommands.rb
@@ -2,7 +2,7 @@ class Gocommands < Formula
   desc "Portable command-line toolkit for iRODS data management service"
   homepage "https://github.com/cyverse/gocommands"
   url "https://github.com/cyverse/gocommands/archive/refs/tags/v0.8.1.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  sha256 "9b9bbc34484413b16af45435258a4a0d7bf33e678718c12bdfc981577da058a1"
   license "BSD-3-Clause"
 
   livecheck do
