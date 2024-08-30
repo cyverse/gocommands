fea55319fea9e3acc62eaf132153ebeeda6ef7d2
Illyoung Choi <iychoi@email.arizona.edu>
2024-08-30 17:17:23 +0000

    Fix application name

--- commons/constant.go
+++ commons/constant.go
@@ -3,15 +3,12 @@ package commons
 import "time"
 
 const (
-	clientProgramName          string        = "md-repo-cli"
+	clientProgramName          string        = "gocommands"
 	connectionTimeout          time.Duration = 10 * time.Minute
-	filesystemTimeout          time.Duration = 10 * time.Minute
 	TransferTreadNumDefault    int           = 5
 	UploadTreadNumMax          int           = 20
 	TcpBufferSizeDefault       int           = 4 * 1024 * 1024
 	TcpBufferSizeStringDefault string        = "4MB"
 
 	RedirectToResourceMinSize int64 = 1024 * 1024 * 1024 // 1GB
-	ParallelUploadMinSize     int64 = 80 * 1024 * 1024   // 80MB
-
 )
