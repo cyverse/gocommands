167fa078bb4a33bba462faf3822ee6e9039221b3
Illyoung Choi <iychoi@email.arizona.edu>
2026-01-08 09:38:31 -0700

    remove unused function

--- commons/terminal/progress.go
+++ commons/terminal/progress.go
@@ -148,20 +148,3 @@ func firstPathSeparatorIndex(p string) int {
 
 	return idx2
 }
-
-func getBasenameOfPath(p string) string {
-	p = strings.TrimRight(p, string(os.PathSeparator))
-	p = strings.TrimRight(p, "/")
-
-	idx1 := strings.LastIndex(p, string(os.PathSeparator))
-	idx2 := strings.LastIndex(p, "/")
-
-	if idx1 < 0 && idx2 < 0 {
-		return p
-	}
-
-	if idx1 >= idx2 {
-		return p[idx1+1:]
-	}
-	return p[idx2+1:]
-}
